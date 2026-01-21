68088d8b6ecaf117eabfe0b3f66c091d5b7a5b0a
rawstar1-alt <siammujahid1@gmail.com>
2026-01-22 02:02:02 +0600

    mvc Architecture

---
+++ view/login1.php
@@ -0,0 +1,31 @@
+
+<?php
+$Name = $_POST['name'];
+$Password = $_POST['password'];
+
+$Con = mysqli_connect('localhost', 'root', '', 'ecommerce');
+
+$result = mysqli_query(
+    $Con,
+    "SELECT * FROM tdluser 
+     WHERE (UserName='$Name' OR Email='$Name') 
+     AND Password='$Password'"
+);
+session_start();
+if (mysqli_num_rows($result) > 0) {
+    $_SESSION['user'] = $Name ;
+    echo "
+    <script>
+        alert('Successfully Login');
+        window.location.href = '../index.php';
+    </script>
+    ";
+} else {
+    echo "
+    <script>
+        alert('incorrect email/username/password');
+        window.location.href = 'login.php';
+    </script>
+    ";
+}
+?>
